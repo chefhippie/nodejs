2a31effc95224353e9386d037165163ad0a1a8b4
Thomas Boerger <thomas@webhippie.de>
2014-10-13 18:38:59 +0200

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: nodejs
+# Attributes:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["nodejs"]["packages"] = value_for_platform_family(
+  "debian" => %w(
+    nodejs
+    nodejs-dev
+    nodejs-legacy
+  ),
+  "ubuntu" => %w(
+    nodejs
+    nodejs-dev
+    npm
+  ),
+  "suse" => %w(
+    nodejs
+    nodejs-devel
+  )
+)
+
+default["nodejs"]["install_npm"] = value_for_platform_family(
+  "debian" => true,
+  "ubuntu" => false,
+  "suse" => false
+)
+
+default["nodejs"]["zypper"]["alias"] = "nodejs"
+default["nodejs"]["zypper"]["title"] = "Node.js"
+default["nodejs"]["zypper"]["repo"] = "http://download.opensuse.org/repositories/devel:/languages:/nodejs/openSUSE_#{node["platform_version"]}/"
+default["nodejs"]["zypper"]["key"] = "#{node["nodejs"]["zypper"]["repo"]}repodata/repomd.xml.key"
---
+++ recipes/default.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: nodejs
+# Recipe:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+case node["platform_family"]
+when "suse"
+  include_recipe "zypper"
+
+  zypper_repository node["nodejs"]["zypper"]["alias"] do
+    uri node["nodejs"]["zypper"]["repo"]
+    key node["nodejs"]["zypper"]["key"]
+    title node["nodejs"]["zypper"]["title"]
+
+    action :add
+  end
+end
+
+node["nodejs"]["packages"].each do |name|
+  package name do
+    action :install
+  end
+end
+
+case node["platform_family"]
+when "debian"
+  remote_file ::File.join(Chef::Config[:file_cache_path], "npm-install") do
+    source "https://www.npmjs.org/install.sh"
+    action :create_if_missing
+
+    only_if do
+      node["nodejs"]["install_npm"]
+    end
+  end
+
+  bash "npm_install" do
+    code <<-EOH
+      clean=yes bash npm-install
+    EOH
+
+    cwd Chef::Config[:file_cache_path]
+    action :run
+
+    not_if do
+      ::File.exists? "/usr/bin/npm"
+    end
+  end
+end
